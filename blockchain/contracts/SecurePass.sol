e6f7e6579c5810c692793d4a1e77312880136210
iao233 <isfaroshir@gmail.com>
2023-05-12 16:25:12 -0400

    submission

--- blockchain/contracts/SecurePass.sol
+++ blockchain/contracts/SecurePass.sol
@@ -4,9 +4,7 @@ pragma solidity >=0.7.0 <0.9.0;
 contract SecurePass {
     //USER STRUCT
     struct user {
-        string email;
-        string name;
-        string securepassword;
+        address userAddress;
         password[] passwords;
     }
 
@@ -18,29 +16,13 @@ contract SecurePass {
 
     mapping(address => user) userList;
 
-    // mapping (bytes32 => password[]) allPasswords;
-
     function checkUserExists(address pubkey) public view returns (bool) {
-        return bytes(userList[pubkey].email).length > 0;
+        return userList[pubkey].userAddress != address(0);
     }
 
-    function createAccount(
-        string calldata email,
-        string calldata name,
-        string calldata securepassword
-    ) external {
+    function createAccount() external {
         require(checkUserExists(msg.sender) == false, "User already exists");
-        require(bytes(email).length > 0, "Email cannot be empty");
-
-        userList[msg.sender].email = email;
-        userList[msg.sender].name = name;
-        userList[msg.sender].securepassword = securepassword;
-    }
-
-    function getUsername(address pubkey) external view returns (string memory) {
-        require(checkUserExists(pubkey), "User is not registered");
-
-        return userList[pubkey].email;
+        userList[msg.sender].userAddress = msg.sender; 
     }
 
     function addPassword(
